b4c17a8eb2f4e19bf87684b9062c6cc35e9f68b5
Eric Chrobak <eric@ericchrobak.com>
2018-04-25 01:22:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
   if array.size == 2
     array.join(" and ")
-  elsif array.size == 3
-    array.join(", ", " and ")
   else
+    words = " and #{array.pop}"
     array.join(", ")
+    array << words
   end
 end
